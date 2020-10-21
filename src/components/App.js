cf3dc8edc006f1063717571b11ba59c9a1fcc8a0
Mohit Gehi <47024623+mohitgehi@users.noreply.github.com>
2020-10-21 22:17:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,8 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = { flag: false };
   }
-
-  state = {
-    text: "",
-  };
 
   render() {
     return (
@@ -17,13 +14,17 @@ class App extends Component {
         <button
           id="click"
           onClick={() => {
-            this.setState(
-              (text: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
-            );
+            this.setState({ flag: true });
           }}
         >
-          <p id="para">{text}</p>
+          click
         </button>
+        {this.state.flag && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
       </div>
     );
   }
